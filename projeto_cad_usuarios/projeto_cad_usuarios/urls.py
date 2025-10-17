16fd2d449370a6520ddb60f3c35e6c2acfeb5623
Sergies Baptista de Oliveira <43007640+SergiesOliveira@users.noreply.github.com>
2025-10-17 15:25:57 -0300

    Aula Cadastro

--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_cad_usuarios import views
 
 urlpatterns = [
+   
+    # rota, view responsável, nome de referência
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('usuarios/', views.usuarios, name='listagem_usuarios'),
+
 ]
